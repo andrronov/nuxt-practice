06ea3c6a32a73ef35caf997edd81392717c2ec12
andrronov <andrew270504cska@gmail.com>
2023-12-07 03:03:27 +0300

    practice, assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from 'path';
+
 export default defineNuxtConfig({
+  alias: {'@': resolve(__dirname, "/")},
+  css: ["./assets/main.scss"],
+  modules: ['@nuxtjs/tailwindcss'],
   devtools: { enabled: true }
 })
